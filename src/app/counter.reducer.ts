dc6dae1c0e50637780c3efe66a052dcc67e3440c
Josh Davis <jldavis33@yahoo.com>
2021-10-14 11:24:05 -0500

    Add typing to the counter reducer

--- src/app/counter.reducer.ts
+++ src/app/counter.reducer.ts
@@ -1,17 +1,20 @@
-import { createReducer, on } from "@ngrx/store";
-import { increment, decrement, reset } from "./counter.actions";
+import { Action, createReducer, on} from "@ngrx/store";
+import {increment, decrement, reset} from "./counter.actions";
 
-export const initialState = 0;
+export const initialState: number = 0;
 
 // Create private reducer function
 const _counterReducer = createReducer(
   initialState,
   on(increment, (state) => state + 1),  // on dispatch trigger of 'increment' ('[Counter Component] Increment'), update `state`
   on(decrement, (state) => state - 1),
-  on(reset, (state) => 0)
+  on(reset, () => 0)
 );
 
-export function counterReducer(state, action) {
+export function counterReducer(
+  state: number,
+  action: Action
+) {
   return _counterReducer(state, action);
 }
 
