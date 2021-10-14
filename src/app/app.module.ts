98014ddc0a1b39dbde4402781b23f87a6c436e36
Josh Davis <jldavis33@yahoo.com>
2021-10-14 13:32:19 -0500

    add `StoreModule` and `counterReducer` to app module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
+import { counterReducer } from "./counter.reducer";
+import { MyCounterComponent } from './my-counter/my-counter.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { StoreModule } from '@ngrx/store';
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot({}, {})
+    StoreModule.forRoot({
+      count: counterReducer
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
